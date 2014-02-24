48bd5d758e61f67597953fcebb097e1ab5f6b0dc
RockxIt <ben@vublessenrooster.be>
2014-02-24 17:38:09 +0100

    Update models.py

--- angular_flask/models.py
+++ angular_flask/models.py
@@ -4,20 +4,9 @@ from angular_flask.core import db
 from angular_flask import app
 
 class Post(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(80))
-	body = db.Column(db.Text)
-	pub_date = db.Column(db.DateTime)
-
-	def __init__(self, title, body, pub_date=None):
-		self.title = title
-		self.body = body
-		if pub_date is None:
-			pub_date = datetime.utcnow()
-		self.pub_date = pub_date
-
-	def __repr__(self):
-		return '<Post %r>' % self.title
+	title = CharField()
+	body = TextField()
+	pub_date = DateTimeField(default = datetime.now())
 
 # models for which we want to create API endpoints
 app.config['API_MODELS'] = { 'post': Post }
