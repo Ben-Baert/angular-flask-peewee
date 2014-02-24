2dcf62afd5cb6d7f5fce7069ef84033c76f3a846
RockxIt <ben@vublessenrooster.be>
2014-02-24 17:42:37 +0100

    Update settings.py

--- angular_flask/settings.py
+++ angular_flask/settings.py
@@ -1,6 +1,8 @@
-
 DEBUG = True
 SECRET_KEY = 'temporary_secret_key' # make sure to change this
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/angular_flask.db'
+DATABASE = {
+    'name': 'example.db',
+    'engine': 'peewee.SqliteDatabase',
+}
 
